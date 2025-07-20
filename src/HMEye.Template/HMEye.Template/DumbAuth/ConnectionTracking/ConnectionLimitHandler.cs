41f5985b5d267c315b57701b71ad97fc4c674d99
99cm234 <99cm234@users.noreply.github.com>
2025-07-20 14:13:04 -0400

    Change ConnectionLimitHandler to set bool flag in OnCircuitOpened and leave Navigate to server-full page in OnConnectionUpAsync - Circuit is what needs to be tracked, but NavigationManager is not reliable until the Connection is up.

--- src/HMEye.Template/HMEye.Template/DumbAuth/ConnectionTracking/ConnectionLimitHandler.cs
+++ src/HMEye.Template/HMEye.Template/DumbAuth/ConnectionTracking/ConnectionLimitHandler.cs
@@ -7,6 +7,7 @@ public class ConnectionLimitHandler : CircuitHandler
 {
 	private readonly ConnectionTracker _tracker;
 	private readonly NavigationManager _navigationManager;
+	private bool _isAllowed;
 
 	public ConnectionLimitHandler(ConnectionTracker tracker, NavigationManager NavigationManager)
 	{
@@ -14,19 +15,28 @@ public class ConnectionLimitHandler : CircuitHandler
 		_navigationManager = NavigationManager;
 	}
 
+	public override Task OnCircuitOpenedAsync(Circuit circuit, CancellationToken cancellationToken)
+	{
+		_isAllowed = _tracker.TryAdd();
+		return base.OnCircuitOpenedAsync(circuit, cancellationToken);
+	}
+
 	public override Task OnConnectionUpAsync(Circuit circuit, CancellationToken cancellationToken)
 	{
-		if (!_tracker.TryAdd())
+		if (!_isAllowed)
 		{
-			_navigationManager.NavigateTo($"/account/server-full?error={_tracker.Count.ToString()}-clients-connected", forceLoad: true);
+			_navigationManager.NavigateTo(
+				$"/account/server-full?error={_tracker.Count}-clients-connected",
+				forceLoad: true
+			);
 		}
 		return base.OnConnectionUpAsync(circuit, cancellationToken);
 	}
 
 	public override Task OnCircuitClosedAsync(Circuit circuit, CancellationToken cancellationToken)
 	{
-		_tracker.Remove();
+		if (_isAllowed)
+			_tracker.Remove();
 		return base.OnCircuitClosedAsync(circuit, cancellationToken);
 	}
 }
-
